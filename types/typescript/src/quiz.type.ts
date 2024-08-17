77cb5635633ba57c5945e2d5696f5672307f38a1
Nicolas Fritz <nicolasfr.privat@gmail.com>
2024-08-18 01:07:04 +0200

    [backend][] added example data to simulate db

--- types/typescript/src/quiz.type.ts
+++ types/typescript/src/quiz.type.ts
@@ -1,11 +1,17 @@
 export type AnswerType = 'a' | 'b' | 'c';
 
+export type QuestionOption = {
+    type: AnswerType;
+    text: string;
+}
+
 export type QuestionData = QuestionHidden & {
     answers: AnswerType;
 }
 
 export type QuestionHidden = {
     id: number;
+    options: QuestionOption[];
     question: string;
 }
 
