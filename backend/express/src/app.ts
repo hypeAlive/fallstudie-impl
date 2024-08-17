04e0d054b3743af15b3c02465eca6df7e5ad402b
Nicolas Fritz <nicolasfr.privat@gmail.com>
2024-08-18 00:28:07 +0200

    init

---
+++ backend/express/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import winston from "winston";
+import helmet from "helmet";
+import cors from "cors";
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console(),
+    ]
+});
+
+export const app = express();
+
+app.set('case sensitive routing', false);
+app.set('trust proxy', 1);
+
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:8080', 'http://localhost:4200' ] }));
+app.options('*', cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(8080, () => {
+   logger.info('Server is running on port 8080');
+});
