e972dc8a1aa91efccb4dbab640df93040d8d2ffb
Nicolas Fritz <nicolasfr.privat@gmail.com>
2024-08-18 18:32:35 +0200

    [frontend][angular] added basic quiz component

--- frontend/angular/src/app/quiz/quiz.component.ts
+++ frontend/angular/src/app/quiz/quiz.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+  question: any;
+  selectedOption: string = '';
+  result: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRandomQuestion();
+  }
+
+  getRandomQuestion() {
+    this.http.get('/api/question').subscribe((data: any) => {
+      this.question = data;
+      this.selectedOption = '';
+      this.result = '';
+    });
+  }
+
+  submitAnswer() {
+    this.http.post('/api/question', { id: this.question.id, answer: this.selectedOption })
+      .subscribe((data: any) => {
+        this.result = data.isCorrect ? 'Correct!' : 'Incorrect!';
+      });
+  }
 }
